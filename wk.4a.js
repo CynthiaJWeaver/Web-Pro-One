db578dc1d9da0cffa5e29ecb499822ba76e603b2
CynthiaJWeaver <59941100+CynthiaJWeaver@users.noreply.github.com>
2020-02-23 18:26:25 -0500

    add trail files

--- wk.4a.js
+++ wk.4a.js
@@ -1,6 +1,31 @@
-function hikeFunction()
+//begin w3schools example
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
+
+
+
+/********work in progress function hikeFunction()
 {
     document.getElementById("hike")=
     "How many days do you want to hike?";
 
-}
+}*/
