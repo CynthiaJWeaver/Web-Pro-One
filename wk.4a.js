57ae9581df2dad20771259f213e854c80fadced2
CynthiaJWeaver <59941100+CynthiaJWeaver@users.noreply.github.com>
2020-02-22 20:23:26 -0500

    changing JavaScript

--- wk.4a.js
+++ wk.4a.js
@@ -3,6 +3,6 @@ var campingNights;
 var tripCost;
 price = 18;
 campingNights = 9;
-tripcost = price+=campingNights;
-var el = document.getElementById("cost");
-el.textContent = $ + totalTrip
+tripCost = price+=campingNights;
+var el = document.getElementById("tripCost");
+el.textContent = $ + "tripCost";
