c8fab71d1631886eba570f05e7b296e29a05f071
CynthiaJWeaver <59941100+CynthiaJWeaver@users.noreply.github.com>
2020-04-22 10:41:07 -0400

    changes

--- wk.7.js
+++ wk.7.js
@@ -1,4 +1,28 @@
-function process_form(){
+
+function Student(fm) {
+    this.need = fm.need.checked;
+    this.noNeed = fm.noNeed.checked;
+    this.grade = fm.grade.value;
+    this.binder = fm.binder.checked;
+    this.notebook = fm.notebook.checked;
+    this.agenda = fm.agenda.checked;
+    this.dividers = fm.dividers.checked;
+    this.paper = fm.paper.checked;
+    this.folder = fm.folder.checked;
+
+    this.recommendation = function () {
+        if
+            (!this.need) {
+            return "Please bring your supplies by the 2nd wee of classes. Thank you!";
+        }
+    }
+}
+function process_form() {
     var fm = document.getElementById("my_form");
-    console.log(fm.grade.value)
+    var out = document.getElementById("output");
+    var student = new Student(fm);
+
+
+    console.log("Debug:" + JSON.stringify(student))
+
 }
